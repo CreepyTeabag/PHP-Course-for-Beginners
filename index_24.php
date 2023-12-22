105862fbf16dd232ce038b0a0799644fdb7f12e6
Koniukhova Sofia <sovunja1712@gmail.com>
2023-12-22 14:06:46 +0300

    Watched lesson 24

--- index_24.php
+++ index_24.php
@@ -8,9 +8,11 @@
 </head>
 <body>
 
-   <?php
-   
-   ?>
+   <form class="searchform" action="search.php" method="post">
+      <label for="search">Search for user :</label>
+      <input id="search" type="text" name="usersearch" placeholder="Search ... ">
+      <button>Search</button>
+   </form>
 
 </body>
 </html>
