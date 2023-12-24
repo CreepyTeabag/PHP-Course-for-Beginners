3de19cd15e4b3623c19072fc96a59c54f2f8daf8
CreepyTeabag <124189089+CreepyTeabag@users.noreply.github.com>
2023-12-24 18:44:02 +0300

    Watched lesson 29

--- Lesson_28/index.php
+++ Lesson_28/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'includes/config_session.inc.php';
 require_once 'includes/signup_view.inc.php';
+require_once 'includes/login_view.inc.php';
 ?>
 
 <!DOCTYPE html>
@@ -13,27 +14,52 @@ require_once 'includes/signup_view.inc.php';
 </head>
 <body>
 
-   <h3>Login</h3>
-
-   <form action="includes/login.inc.php" method="post">
-      <input type="text" name="username" placeholder="Username">
-      <input type="password" name="pwd" placeholder="Password">
-      <button>Login</button>
-   </form>
-
-
-   <h3>Signup</h3>
-
-   <form action="includes/signup.inc.php" method="post">
-      <?php 
-      signup_inputs();
+   <h3>
+      <?php
+      output_username();
       ?>
-      <button>Signup</button>
-   </form>
+   </h3>
+
+   <?php
+   if (!isset($_SESSION["user_id"])) { ?>
+      <h3>Login</h3>
+
+      <form action="includes/login.inc.php" method="post">
+         <input type="text" name="username" placeholder="Username">
+         <input type="password" name="pwd" placeholder="Password">
+         <button>Login</button>
+      </form>
+   <?php } ?> 
+
+   <?php
+   check_login_errors();
+   ?>
+
+
+   <?php
+   if (!isset($_SESSION["user_id"])) { ?>
+      <h3>Signup</h3>
+
+      <form action="includes/signup.inc.php" method="post">
+         <?php 
+         signup_inputs();
+         ?>
+         <button>Signup</button>
+      </form>
+   <?php } ?>
 
    <?php
    check_signup_errors();
    ?>
+
+   <?php
+   if (isset($_SESSION["user_id"])) { ?>
+      <h3>Logout</h3>
+
+      <form action="includes/logout.inc.php" method="post">
+         <button>Logout</button>
+      </form>
+   <?php } ?>
 
 </body>
 </html>
